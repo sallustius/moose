26056a14b40df513d5b8656e0171f67d3c36bb77
Alex Lindsay <alexlindsay239@gmail.com>
2018-06-04 08:29:43 -0600

    Add exodiff test.

--- modules/contact/src/LM.C
+++ modules/contact/src/LM.C
@@ -26,9 +26,6 @@ validParams<LM>()
   MooseEnum orders("FIRST SECOND THIRD FOURTH", "FIRST");
   params.addParam<MooseEnum>("order", orders, "The finite element order used for projections");
   params.set<bool>("use_displaced_mesh") = true;
-  params.addCoupledVar("disp_x", "The x displacement");
-  params.addCoupledVar("disp_y", "The y displacement");
-  params.addCoupledVar("disp_z", "The z displacement");
   return params;
 }
 
@@ -37,10 +34,7 @@ LM::LM(const InputParameters & parameters)
     _penetration_locator(
         getPenetrationLocator(getParam<BoundaryName>("master"),
                               getParam<BoundaryName>("slave"),
-                              Utility::string_to_enum<Order>(getParam<MooseEnum>("order")))),
-    _disp_x_id(coupled("disp_x")),
-    _disp_y_id(coupled("disp_y")),
-    _disp_z_id(coupled("disp_z"))
+                              Utility::string_to_enum<Order>(getParam<MooseEnum>("order"))))
 {
 }
 
@@ -78,36 +72,4 @@ LM::computeQpJacobian()
     }
   }
   return 0;
-}
-
-Real
-LM::computeQpOffDiagJacobian(unsigned jvar)
-{
-  if (jvar == _disp_x_id || jvar == _disp_y_id || jvar == _disp_z_id)
-  {
-    std::map<dof_id_type, PenetrationInfo *>::iterator found =
-        _penetration_locator._penetration_info.find(_current_node->id());
-    if (found != _penetration_locator._penetration_info.end())
-    {
-      PenetrationInfo * pinfo = found->second;
-      if (pinfo != NULL)
-      {
-        const Elem * master_elem = pinfo->_elem;
-        unsigned master_side = pinfo->_side_num;
-
-        /*
-         * Make sure proper values of test and shape functions are in _test and _phi
-         */
-        std::vector<Point> points = {pinfo->_closest_point};
-
-        // reinit variables on the master element's face at the contact point
-        _fe_problem.setNeighborSubdomainID(master_elem, 0);
-        _fe_problem.reinitNeighborPhys(master_elem, master_side, points, 0);
-
-        MooseVariableFEBase & var = _sys.getVariable(0, jvar);
-        std::vector<dof_id_type> master_dof_indices;
-        var.getDofIndices(master_elem, dof_indices);
-      }
-    }
-  }
 }
