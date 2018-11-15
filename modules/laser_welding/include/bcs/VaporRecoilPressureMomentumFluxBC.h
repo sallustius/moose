041ef002aad3d78f9058f47bec67abc0c91cedd8
Alex Lindsay <alexlindsay239@gmail.com>
2018-11-15 11:21:54 -0700

    Add VaporRecoilPressureMomentumFluxBC

---
+++ modules/laser_welding/include/bcs/VaporRecoilPressureMomentumFluxBC.h
@@ -0,0 +1,46 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef VAPORRECOILPRESSUREMOMENTUMFLUXBC_H
+#define VAPORRECOILPRESSUREMOMENTUMFLUXBC_H
+
+#include "ADNodalBC.h"
+
+template <ComputeStage compute_stage>
+class VaporRecoilPressureMomentumFluxBC;
+
+declareADValidParams(VaporRecoilPressureMomentumFluxBC);
+
+template <ComputeStage compute_stage>
+class VaporRecoilPressureMomentumFluxBC : public ADNodalBC<compute_stage>
+{
+public:
+  VaporRecoilPressureMomentumFluxBC(const InputParameters & parameters);
+
+protected:
+  virtual typename Moose::RealType<compute_stage>::type computeQpResidual() override;
+
+  usingNodalBCMembers;
+
+  const Real _ap0;
+  const Real _ap1;
+  const Real _ap2;
+  const Real _ap3;
+  const Real _bp0;
+  const Real _bp1;
+  const Real _bp2;
+  const Real _bp3;
+  const Real _Tb;
+  const Real _Tbound1;
+  const Real _Tbound2;
+
+  const typename Moose::RealType<compute_stage>::type & _temperature;
+};
+
+#endif /* VAPORRECOILPRESSUREMOMENTUMFLUXBC_H */
