09ecb6c76e5f41d924149630eb48a4d139af62be
Alex Lindsay <alexlindsay239@gmail.com>
2018-12-17 11:48:16 -0700

    Add SurfaceTensionBCs: laser welding

---
+++ modules/laser_welding/include/bcs/SurfaceTensionBC.h
@@ -0,0 +1,35 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef SURFACETENSIONBC_H
+#define SURFACETENSIONBC_H
+
+#include "ADIntegratedBC.h"
+
+template <ComputeStage compute_stage>
+class SurfaceTensionBC;
+
+declareADValidParams(SurfaceTensionBC);
+
+template <ComputeStage compute_stage>
+class SurfaceTensionBC : public ADIntegratedBC<compute_stage>
+{
+public:
+  SurfaceTensionBC(const InputParameters & parameters);
+
+protected:
+  virtual ADResidual computeQpResidual() override;
+
+  usingIntegratedBCMembers;
+
+  const unsigned _component;
+  const ADMaterialProperty(RealVectorValue) & _grad_surface_tension;
+};
+
+#endif /* SURFACETENSIONBC_H */
---
+++ modules/laser_welding/src/bcs/SurfaceTensionBC.C
@@ -0,0 +1,37 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "SurfaceTensionBC.h"
+
+registerADMooseObject("LaserWeldingApp", SurfaceTensionBC);
+
+defineADValidParams(SurfaceTensionBC,
+                    ADIntegratedBC,
+                    params.addClassDescription("Surface tension stresses.");
+                    params.addRequiredParam<unsigned>("component", "The velocity component");
+                    params.addParam<MaterialPropertyName>("grad_surface_tension_name",
+                                                          "grad_surface_tension",
+                                                          "The gradient of the surface tension"););
+
+template <ComputeStage compute_stage>
+SurfaceTensionBC<compute_stage>::SurfaceTensionBC(const InputParameters & parameters)
+  : ADIntegratedBC<compute_stage>(parameters),
+    _component(adGetParam<unsigned>("component")),
+    _grad_surface_tension(adGetADMaterialProperty<RealVectorValue>("grad_surface_tension_name"))
+{
+}
+
+template <ComputeStage compute_stage>
+ADResidual
+SurfaceTensionBC<compute_stage>::computeQpResidual()
+{
+  return -_test[_i][_qp] *
+         (_grad_surface_tension[_qp](_component) -
+          _normals[_qp](_component) * (_normals[_qp] * _grad_surface_tension[_qp]));
+}
