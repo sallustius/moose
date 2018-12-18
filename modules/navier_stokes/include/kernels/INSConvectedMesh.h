8d463666e0b23ec3d2d64b1997a1f235da4d9f72
Alex Lindsay <alexlindsay239@gmail.com>
2018-12-18 14:01:31 -0700

    Convert INSConvectedMesh to AD: navier-stokes/laser-welding

--- modules/navier_stokes/include/kernels/INSConvectedMesh.h
+++ modules/navier_stokes/include/kernels/INSConvectedMesh.h
@@ -10,38 +10,33 @@
 #ifndef INSCONVECTEDMESH_H
 #define INSCONVECTEDMESH_H
 
-#include "Kernel.h"
+#include "ADTimeKernel.h"
 
 // Forward Declaration
+template <ComputeStage>
 class INSConvectedMesh;
 
-template <>
-InputParameters validParams<INSConvectedMesh>();
+declareADValidParams(INSConvectedMesh);
 
 /**
  * This calculates the time derivative for a coupled variable
  **/
-class INSConvectedMesh : public Kernel
+template <ComputeStage compute_stage>
+class INSConvectedMesh : public ADTimeKernel<compute_stage>
 {
 public:
   INSConvectedMesh(const InputParameters & parameters);
 
 protected:
-  virtual Real computeQpResidual() override;
-  virtual Real computeQpJacobian() override;
-  virtual Real computeQpOffDiagJacobian(unsigned int jvar) override;
+  virtual ADResidual computeQpResidual() override;
 
-  const VariableValue & _disp_x_dot;
-  const VariableValue & _d_disp_x_dot;
-  const unsigned int _disp_x_id;
-  const VariableValue & _disp_y_dot;
-  const VariableValue & _d_disp_y_dot;
-  const unsigned int _disp_y_id;
-  const VariableValue & _disp_z_dot;
-  const VariableValue & _d_disp_z_dot;
-  const unsigned int _disp_z_id;
+  const ADVariableValue & _disp_x_dot;
+  const ADVariableValue & _disp_y_dot;
+  const ADVariableValue & _disp_z_dot;
 
-  const MaterialProperty<Real> & _rho;
+  const ADMaterialProperty(Real) & _rho;
+
+  usingTimeKernelMembers;
 };
 
 #endif // INSCONVECTEDMESH}_H
--- modules/navier_stokes/src/kernels/INSConvectedMesh.C
+++ modules/navier_stokes/src/kernels/INSConvectedMesh.C
@@ -9,60 +9,38 @@
 
 #include "INSConvectedMesh.h"
 
-registerMooseObject("MooseApp", INSConvectedMesh);
+registerADMooseObject("MooseApp", INSConvectedMesh);
 
-template <>
-InputParameters
-validParams<INSConvectedMesh>()
-{
-  InputParameters params = validParams<Kernel>();
-  params.addClassDescription(
-      "Corrects the convective derivative for situations in which the fluid mesh is dynamic.");
-  params.addRequiredCoupledVar("disp_x", "The x displacement");
-  params.addCoupledVar("disp_y", "The y displacement");
-  params.addCoupledVar("disp_z", "The z displacement");
-  params.addParam<MaterialPropertyName>("rho_name", "rho", "The name of the density");
-  return params;
-}
+defineADValidParams(
+    INSConvectedMesh,
+    ADTimeKernel,
+    params.addClassDescription(
+        "Corrects the convective derivative for situations in which the fluid mesh is dynamic.");
+    params.addRequiredCoupledVar("disp_x", "The x displacement");
+    params.addCoupledVar("disp_y", "The y displacement");
+    params.addCoupledVar("disp_z", "The z displacement");
+    params.addParam<MaterialPropertyName>("rho_name", "rho", "The name of the density");
+    params.addCoupledVar("temperature",
+                         "The temperature on which material properties may depend. If properties "
+                         "do depend on temperature, this variable must be coupled in in order to "
+                         "correctly resize the element matrix"););
 
-INSConvectedMesh::INSConvectedMesh(const InputParameters & parameters)
-  : Kernel(parameters),
-    _disp_x_dot(coupledDot("disp_x")),
-    _d_disp_x_dot(coupledDotDu("disp_x")),
-    _disp_x_id(coupled("disp_x")),
-    _disp_y_dot(isCoupled("disp_y") ? coupledDot("disp_y") : _zero),
-    _d_disp_y_dot(isCoupled("disp_y") ? coupledDotDu("disp_y") : _zero),
-    _disp_y_id(coupled("disp_y")),
-    _disp_z_dot(isCoupled("disp_z") ? coupledDot("disp_z") : _zero),
-    _d_disp_z_dot(isCoupled("disp_z") ? coupledDotDu("disp_z") : _zero),
-    _disp_z_id(coupled("disp_z")),
-    _rho(getMaterialProperty<Real>("rho_name"))
+template <ComputeStage compute_stage>
+INSConvectedMesh<compute_stage>::INSConvectedMesh(const InputParameters & parameters)
+  : ADTimeKernel<compute_stage>(parameters),
+    _disp_x_dot(adCoupledDot("disp_x")),
+    _disp_y_dot(isCoupled("disp_y") ? adCoupledDot("disp_y") : adZeroValue()),
+    _disp_z_dot(isCoupled("disp_z") ? adCoupledDot("disp_z") : adZeroValue()),
+    _rho(adGetADMaterialProperty<Real>("rho_name"))
 {
 }
 
-Real
-INSConvectedMesh::computeQpResidual()
+template <ComputeStage compute_stage>
+ADResidual
+INSConvectedMesh<compute_stage>::computeQpResidual()
 {
   return _test[_i][_qp] * -_rho[_qp] *
-         RealVectorValue(_disp_x_dot[_qp], _disp_y_dot[_qp], _disp_z_dot[_qp]) * _grad_u[_qp];
-}
-
-Real
-INSConvectedMesh::computeQpJacobian()
-{
-  return _test[_i][_qp] * -_rho[_qp] *
-         RealVectorValue(_disp_x_dot[_qp], _disp_y_dot[_qp], _disp_z_dot[_qp]) * _grad_phi[_j][_qp];
-}
-
-Real
-INSConvectedMesh::computeQpOffDiagJacobian(unsigned int jvar)
-{
-  if (jvar == _disp_x_id)
-    return _test[_i][_qp] * -_rho[_qp] * _phi[_j][_qp] * _d_disp_x_dot[_qp] * _grad_u[_qp](0);
-  else if (jvar == _disp_y_id)
-    return _test[_i][_qp] * -_rho[_qp] * _phi[_j][_qp] * _d_disp_y_dot[_qp] * _grad_u[_qp](1);
-  else if (jvar == _disp_z_id)
-    return _test[_i][_qp] * -_rho[_qp] * _phi[_j][_qp] * _d_disp_z_dot[_qp] * _grad_u[_qp](2);
-  else
-    return 0.0;
+         VectorValue<typename Moose::RealType<compute_stage>::type>(
+             _disp_x_dot[_qp], _disp_y_dot[_qp], _disp_z_dot[_qp]) *
+         _grad_u[_qp];
 }
