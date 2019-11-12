8c8457bf8fff351b466a69e51d22e64817ef398e
Alex Lindsay <alexlindsay239@gmail.com>
2019-11-12 09:09:06 -0800

    Don't call computeQpSlaveValue for RANFS

--- framework/include/constraints/NodeFaceConstraint.h
+++ framework/include/constraints/NodeFaceConstraint.h
@@ -46,7 +46,7 @@ public:
   /**
    * Compute the value the slave node should have at the beginning of a timestep.
    */
-  void computeSlaveValue(NumericVector<Number> & current_solution);
+  virtual void computeSlaveValue(NumericVector<Number> & current_solution);
 
   /**
    * Computes the residual Nodal residual.
