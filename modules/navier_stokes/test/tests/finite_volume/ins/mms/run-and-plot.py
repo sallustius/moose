dac0dbc7cce7409bf8ac5e7eb3e8bc4d46ad1ccb
Alex Lindsay <alexlindsay239@gmail.com>
2020-08-24 15:29:55 -0700

    Verify INS in 2D

--- modules/navier_stokes/test/tests/finite_volume/ins/mms/run-and-plot.py
+++ modules/navier_stokes/test/tests/finite_volume/ins/mms/run-and-plot.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import mms
-df1 = mms.run_spatial('lid-driven.i', 9, y_pp=['L2u', 'L2p'])
+df1 = mms.run_spatial('lid-driven.i', 7, y_pp=['L2u', 'L2v', 'L2p'])
 
 fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
-fig.plot(df1, label=['L2u', 'L2p'], marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
+fig.plot(df1, label=['L2u', 'L2v', 'L2p'], marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
 fig.save('lid-driven.png')
