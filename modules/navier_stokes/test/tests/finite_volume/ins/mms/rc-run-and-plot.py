4f64a841d10b518258c896ef425ea32fedfb0cfa
Alex Lindsay <alexlindsay239@gmail.com>
2020-08-25 10:37:37 -0700

    Allow user to control amount of ghost layers for FV

--- modules/navier_stokes/test/tests/finite_volume/ins/mms/rc-run-and-plot.py
+++ modules/navier_stokes/test/tests/finite_volume/ins/mms/rc-run-and-plot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import mms
-df1 = mms.run_spatial('rc.i', 6, y_pp=['L2u', 'L2v', 'L2p'])
+df1 = mms.run_spatial('rc.i', 8, y_pp=['L2u', 'L2v', 'L2p'])
 
 fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
 fig.plot(df1, label=['L2u', 'L2v', 'L2p'], marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
