2613727274c3cf5b99c4a67630d1356cc869359d
Alex Lindsay <alexlindsay239@gmail.com>
2020-08-26 08:41:27 -0700

    Verify 1D RZ INS with average interpolation

---
+++ modules/navier_stokes/test/tests/finite_volume/ins/mms/cylindrical/1d-average-run-and-plot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import mms
+
+labels = ['L2u', 'L2p']
+df1 = mms.run_spatial('1d-average.i', 7, y_pp=labels)
+
+fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
+fig.plot(df1, label=labels, marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
+fig.save('1d-average.png')
