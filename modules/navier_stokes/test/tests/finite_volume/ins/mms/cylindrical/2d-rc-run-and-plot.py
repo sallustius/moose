147251c05f8dfe3a0bf74b11381ed93ec177c5dd
Alex Lindsay <alexlindsay239@gmail.com>
2020-08-26 17:04:20 -0700

    Verify 2D RZ RC with dirichlet bcs

---
+++ modules/navier_stokes/test/tests/finite_volume/ins/mms/cylindrical/2d-rc-run-and-plot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import mms
+
+labels = ['L2u', 'L2v', 'L2p']
+df1 = mms.run_spatial('2d-rc.i', 8, y_pp=labels)
+
+fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
+fig.plot(df1, label=labels, marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
+fig.save('2d-rc.png')
