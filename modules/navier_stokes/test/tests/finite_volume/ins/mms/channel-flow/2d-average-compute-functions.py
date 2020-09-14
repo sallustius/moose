f42cacdbe1364778ed970a7372f813866d232fb1
Alex Lindsay <alexlindsay239@gmail.com>
2020-09-14 11:38:35 -0700

    MMS tests for INSFV 3 eqns (now including energy)

--- modules/navier_stokes/test/tests/finite_volume/ins/mms/channel-flow/2d-average-compute-functions.py
+++ modules/navier_stokes/test/tests/finite_volume/ins/mms/channel-flow/2d-average-compute-functions.py
@@ -9,9 +9,12 @@ vel = u + '* e_i + ' + v + ' * e_j'
 
 p = 'cos(pi * x / 4) * sin(3 * pi * y / 2)'
 
+temp = 'sin(pi * x / 4) * cos(pi * y / 2)'
+
 f_u, e_u = mms.evaluate('div(vel*rho*u) - div(mu * grad(u)) + grad(p).dot(e_i)', u, variable='u', vel=vel, p=p, scalars=['mu', 'rho'])
 f_v, e_v = mms.evaluate('div(vel*rho*v) - div(mu * grad(v)) + grad(p).dot(e_j)', v, variable='v', vel=vel, p=p, scalars=['mu', 'rho'])
 f_p, e_p = mms.evaluate('div(vel)', p, variable='p', vel=vel)
+f_t, e_t = mms.evaluate('div(vel*rho*cp*temp) - div(k * grad(temp))', temp, variable='temp', vel=vel, scalars=['rho', 'cp', 'k'])
 
 rho = sympy.Symbol('rho')
 
@@ -28,3 +31,6 @@ mms.print_hit(f_v, 'forcing_v', mu='${mu}', rho='${rho}')
 
 mms.print_hit(e_p, 'exact_p')
 mms.print_hit(f_p, 'forcing_p')
+
+mms.print_hit(e_t, 'exact_t')
+mms.print_hit(f_t, 'forcing_t', k='${k}', rho='${rho}', cp='${cp}')
